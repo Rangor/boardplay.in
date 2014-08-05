e1c3fdd9d666cc8ac475d428ee716381f6fc9870
Martin Syvertsen <marsyv@signicat.com>
2014-08-05 16:23:18 +0200

    Simple DB working

--- im_playing.js
+++ im_playing.js
@@ -1,7 +1,29 @@
 var mongoose = require('mongoose');
+var Game = require("./models").Game;
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-    console.log("Success");
+
+    Game.find(function (err, games) {
+      if (err) return console.error(err);
+      console.log(games);
+
+      // games.[0].delete(function(err, games[0]){
+      //   if(err) return console.error(err);
+      // })
+
+      process.exit(code=0);
+    })
 });
+
+
+
+
+
+// var catan = new Game({ name: 'Catan' })
+// console.log(catan.name) // 'Catan'
+//
+// catan.save(function (err, catan) {
+//   if (err) return console.error(err);
+// });
