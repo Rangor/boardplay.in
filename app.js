7aa2449a3a0d375709d4e1348875002c4a952972
Martin Syvertsen <marsyv@signicat.com>
2014-08-08 23:56:47 +0200

    Heroku config stuff

--- app.js
+++ app.js
@@ -6,10 +6,36 @@ var express = require('express');
 var hash = require('./pass').hash;
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var mongoose = require('mongoose');
 var Game = require("./models").Game;
 var Play = require("./models").Play;
-mongoose.connect('mongodb://localhost/test');
+
+
+var http = require ('http');             // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+// mongoose.connect('mongodb://localhost/test');
+
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
