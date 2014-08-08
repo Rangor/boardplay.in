93dcd99b9596eb494b1e9c6bc61e338c8f391050
Martin Syvertsen <marsyv@signicat.com>
2014-08-08 19:22:09 +0200

    Now can add games

--- models.js
+++ models.js
@@ -4,6 +4,27 @@ var gameSchema = mongoose.Schema({
     name: String
 })
 
+var playSchema = mongoose.Schema({
+    userName: String,
+    userId: String,
+    gameName: String,
+    gameId,
+    date: {type: Date, default: Date.now}
+})
+
+new Schema({
+  title:  String,
+  author: String,
+  body:   String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs:  Number
+  }
+});
+
 var Game = mongoose.model('Game', gameSchema);
 
 module.exports = {
