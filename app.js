36be9db1fa6fe42eda5a5f3273fed725ce90c5fd
Martin Syvertsen <marsyv@signicat.com>
2014-08-17 19:50:39 +0200

    Basic menu in place

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.set('views', __dirname + '/views');
 
 app.configure(function(){
   app.use('/css', express.static(__dirname + '/css'));
+  app.use('/js', express.static(__dirname + '/js'));
   app.use(express.static(__dirname + '/public'));
 });
 
